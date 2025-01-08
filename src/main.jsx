609d2311cced48fcd0b617a770b429e9c201a821
kvang2 <kevinvaj88@hotmail.com>
2025-01-08 12:38:19 -0600

    Rendered Hello Atlas correctly

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,18 @@
 import "./index.css";
+import React from "react";
+import ReactDom from "react-dom/client";
+import App from './App';
 
 // Render the App component to the root element
+
+// Get root element
+const rootElement = document.getElementById("root");
+
+// Create React root
+const root = ReactDom.createRoot(rootElement);
+
+root.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+);
